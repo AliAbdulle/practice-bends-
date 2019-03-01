7b20a75624caf0da3db6315c729e57807409fe87
AliAbdulle <alidhobey@gmail.com>
2019-02-28 19:49:06 -0600

    Created new repo for band

---
+++ src/scripts/main.js
@@ -0,0 +1,16 @@
+let bandNumber = 1
+
+const takeNumber = function (bandName) {
+    /*
+        Write your awesome code here. See comments
+        below for what should be returned.
+    */
+   console.log(`${bandNumber}. ${bandName}`)
+   bandNumber++;
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
